4e770edbfa6a5fb39fca6d06d677c8466ebfd20b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:01 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,49 +1,45 @@
-output "data_factory_trigger_custom_events" {
-  description = "All data_factory_trigger_custom_event resources"
-  value       = azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events
-}
 output "data_factory_trigger_custom_events_activated" {
-  description = "List of activated values across all data_factory_trigger_custom_events"
-  value       = [for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : v.activated]
+  description = "Map of activated values across all data_factory_trigger_custom_events, keyed the same as var.data_factory_trigger_custom_events"
+  value       = { for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : k => v.activated }
 }
 output "data_factory_trigger_custom_events_additional_properties" {
-  description = "List of additional_properties values across all data_factory_trigger_custom_events"
-  value       = [for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : v.additional_properties]
+  description = "Map of additional_properties values across all data_factory_trigger_custom_events, keyed the same as var.data_factory_trigger_custom_events"
+  value       = { for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : k => v.additional_properties }
 }
 output "data_factory_trigger_custom_events_annotations" {
-  description = "List of annotations values across all data_factory_trigger_custom_events"
-  value       = [for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : v.annotations]
+  description = "Map of annotations values across all data_factory_trigger_custom_events, keyed the same as var.data_factory_trigger_custom_events"
+  value       = { for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : k => v.annotations }
 }
 output "data_factory_trigger_custom_events_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_trigger_custom_events"
-  value       = [for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_trigger_custom_events, keyed the same as var.data_factory_trigger_custom_events"
+  value       = { for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : k => v.data_factory_id }
 }
 output "data_factory_trigger_custom_events_description" {
-  description = "List of description values across all data_factory_trigger_custom_events"
-  value       = [for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : v.description]
+  description = "Map of description values across all data_factory_trigger_custom_events, keyed the same as var.data_factory_trigger_custom_events"
+  value       = { for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : k => v.description }
 }
 output "data_factory_trigger_custom_events_eventgrid_topic_id" {
-  description = "List of eventgrid_topic_id values across all data_factory_trigger_custom_events"
-  value       = [for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : v.eventgrid_topic_id]
+  description = "Map of eventgrid_topic_id values across all data_factory_trigger_custom_events, keyed the same as var.data_factory_trigger_custom_events"
+  value       = { for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : k => v.eventgrid_topic_id }
 }
 output "data_factory_trigger_custom_events_events" {
-  description = "List of events values across all data_factory_trigger_custom_events"
-  value       = [for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : v.events]
+  description = "Map of events values across all data_factory_trigger_custom_events, keyed the same as var.data_factory_trigger_custom_events"
+  value       = { for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : k => v.events }
 }
 output "data_factory_trigger_custom_events_name" {
-  description = "List of name values across all data_factory_trigger_custom_events"
-  value       = [for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : v.name]
+  description = "Map of name values across all data_factory_trigger_custom_events, keyed the same as var.data_factory_trigger_custom_events"
+  value       = { for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : k => v.name }
 }
 output "data_factory_trigger_custom_events_pipeline" {
-  description = "List of pipeline values across all data_factory_trigger_custom_events"
-  value       = [for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : v.pipeline]
+  description = "Map of pipeline values across all data_factory_trigger_custom_events, keyed the same as var.data_factory_trigger_custom_events"
+  value       = { for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : k => v.pipeline }
 }
 output "data_factory_trigger_custom_events_subject_begins_with" {
-  description = "List of subject_begins_with values across all data_factory_trigger_custom_events"
-  value       = [for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : v.subject_begins_with]
+  description = "Map of subject_begins_with values across all data_factory_trigger_custom_events, keyed the same as var.data_factory_trigger_custom_events"
+  value       = { for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : k => v.subject_begins_with }
 }
 output "data_factory_trigger_custom_events_subject_ends_with" {
-  description = "List of subject_ends_with values across all data_factory_trigger_custom_events"
-  value       = [for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : v.subject_ends_with]
+  description = "Map of subject_ends_with values across all data_factory_trigger_custom_events, keyed the same as var.data_factory_trigger_custom_events"
+  value       = { for k, v in azurerm_data_factory_trigger_custom_event.data_factory_trigger_custom_events : k => v.subject_ends_with }
 }
 
